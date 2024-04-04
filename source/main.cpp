385dcbc5021acab82ad1b641d215bcd9ab737dc6
Daya Darsan <dayadarsan1802@gmail.com>
2024-04-04 16:43:52 +0530

    Add external dependencies and simple main loop with GUI

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 
+#include <GLAD/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui.h>
+
 int main ()
 {
-	std::cout << "Hello World!" << std::endl;
+	auto glfwInitResult { glfwInit () };
+	std::cout << "GLFW initialization " << ( glfwInitResult ? "successful" : "failed" ) << std::endl;
+
+	GLFWwindow * window { glfwCreateWindow ( 1280, 720, "MinePP", nullptr, nullptr ) };
+	glfwMakeContextCurrent ( window );
+
+	auto gladInitResult { gladLoadGL () };
+	std::cout << "GLAD initialization " << ( gladInitResult ? "successful" : "failure" ) << std::endl;
+
+	auto imguiContext { ImGui::CreateContext () };
+	ImGui_ImplOpenGL3_Init ();
+	ImGui_ImplGlfw_InitForOpenGL ( window, true );
+
+	while ( ! glfwWindowShouldClose ( window ) )
+	{
+		glfwPollEvents ();
+
+		glClearColor ( 0.0f, 1.0f, 0.0f, 1.0f );
+		glClear ( GL_COLOR_BUFFER_BIT );
+
+		ImGui_ImplGlfw_NewFrame ();
+		ImGui_ImplOpenGL3_NewFrame ();
+		
+		ImGui::NewFrame ();
+			ImGui::ShowDemoWindow ();
+		ImGui::EndFrame ();
+		
+		ImGui::Render ();
+		ImGui_ImplOpenGL3_RenderDrawData ( ImGui::GetDrawData () );
+		
+		glfwSwapBuffers ( window );
+	}
+
+	ImGui_ImplOpenGL3_Shutdown ();
+	ImGui_ImplGlfw_Shutdown ();
+	
+	ImGui::DestroyContext ( imguiContext );
+
+	glfwTerminate ();
 }
